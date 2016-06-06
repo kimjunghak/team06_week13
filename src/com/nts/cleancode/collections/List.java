b2c181f5bd20c5d5e426134d3cc04b96cf1dfe1c
kimjunghak <junghak92@naver.com>
2016-06-06 17:13:04 +0900

    8. Set의 필요없는 addAll 제거

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -2,10 +2,12 @@ package com.nts.cleancode.collections;
 
 public class List extends AbstractCollection {
 	public void set(int i, Object value) {
-		if (!readOnly) {
-			if (i >= size)
-				throw new ArrayIndexOutOfBoundsException();
-			elements[i] = value;
-		}
+		if (readOnly)
+			return;
+
+		if (i >= size)
+			throw new ArrayIndexOutOfBoundsException();
+		
+		elements[i] = value;
 	}
 }
--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -8,11 +8,4 @@ public class Set extends AbstractCollection {
 		super.add(element);
 
 	}
-
-	public void addAll(AbstractCollection l) {
-		for (int i = 0; i < l.size(); i++) {
-			if (!contains(l.getElementAt(i)))
-				elements[size++] = l.getElementAt(i);
-		}
-	}
 }
